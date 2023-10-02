85b0e3c1eec94bc456b5e8142154057df8436ac5
MrZebarth <kris.zebarth@ocsb.ca>
2023-10-02 10:03:05 -0400

    Initial commit

---
+++ src/j2020/J1.java
@@ -0,0 +1,10 @@
+package j2020;
+
+public class J1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/j2022/J1.java
@@ -0,0 +1,10 @@
+package j2022;
+
+public class J1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
