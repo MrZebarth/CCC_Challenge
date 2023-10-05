fa49a6bd4cea8c1d46f02580e96b0d525229c9c0
MrZebarth <kris.zebarth@ocsb.ca>
2023-10-05 11:16:00 -0400

    I did stuff

--- src/j2020/J1.java
+++ src/j2020/J1.java
@@ -1,10 +1,21 @@
 package j2020;
+import java.util.Scanner;
 
 public class J1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner (System.in);
+		int S = in.nextInt();
+		int M = in.nextInt();
+		int L = in.nextInt();
+		int score = S+2*M+3*L;
+		
+		if (score>=10) {
+			System.out.println("happy");
+		}else {
+			System.out.println("sad");
+		}
 	}
 
 }
--- src/j2021/J1.java
+++ src/j2021/J1.java
@@ -1,10 +1,22 @@
 package j2021;
-
+import java.util.Scanner;
 public class J1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int B = in.nextInt();
+		int P = 5*B-400;
+		System.out.println(P);
+		if (P>100) {
+			System.out.println(-1);
+		}else if (P<100) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+		
+		in.close();
 	}
 
 }
